e94c95b73ce69cd8b55e8a305084af7f57d13cb0
weitian <weitian.bnu@gmail.com>
2025-05-03 20:09:45 +0800

    init grpo train script

---
+++ src/reward_score/gsm8k.py
@@ -0,0 +1,63 @@
+import re
+
+
+def extract_solution(solution_str):
+    """
+    Extract the solution from the given solution string.
+    Args:
+        solution_str (str): The solution string to be processed.
+    Returns:
+        str: The extracted solution.
+    """
+    solution = re.search(r'<answer>(.*?)</answer>', solution_str, re.DOTALL)
+    if solution:
+        return solution.group(1).strip().replace(",", "").replace("$", "")
+
+
+def compute_score_answer(solution_str, ground_truth, match_score=1.0):
+    """
+    Compute the score for a given solution string and ground truth.
+    Args:
+        solution_str (str): The solution string to be evaluated.
+        ground_truth (str): The correct answer to compare against.
+        match_score (float): The score to assign for a correct answer.
+    Returns:
+        float: The computed score.
+    """
+    final_solution = extract_solution(solution_str)
+    return match_score if final_solution == ground_truth else 0.0
+    
+
+def compute_score_CoT_format(solution_str, match_score=1.0):
+    """
+    Compute the score for a given solution string based on its format.
+    Args:
+        solution_str (str): The solution string to be evaluated.
+        match_score (float): The score to assign for a correct format.
+    Returns:
+        float: The computed format score.
+    """
+    format_match = re.search(
+        r'<think>.*?</think>\s*'
+        r'<answer>.*?</answer>\s*',
+        solution_str,
+        re.DOTALL
+    )
+    return match_score if format_match else 0.0
+
+
+def compute_CoT_score(data_source, solution_str, ground_truth, extra_info=None):
+    """
+    Compute the score for a given solution string based on its content and format.
+    Args:
+        data_source (str): The source of the data.
+        solution_str (str): The solution string to be evaluated.
+        ground_truth (str): The correct answer to compare against.
+        extra_info (dict, optional): Additional information for debugging or logging.
+    Returns:
+        float: The computed score.
+    """
+    format_score = compute_score_CoT_format(solution_str, match_score=1.0)
+    answer_score = compute_score_answer(solution_str, ground_truth, match_score=2.0)
+
+    return format_score + answer_score
---
+++ train.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -x
+
+
+# 0. Default parameters
+EXPERIMENT_NAME=${EXPERIMENT_NAME:-"CoT"}  # Experiment name, e.g., "CoT", "WHWM"
+DATASET_NAME=${DATASET_NAME:-"gsm8k"}  # Dataset name, e.g., "gsm8k", "aqua", "math"
+MODEL_PATH=${MODEL_PATH:-"Qwen/Qwen2.5-7B-Instruct"}
+SAVE_CHECKPOINT_PATH=${SAVE_CHECKPOINT_PATH:-"/models/${EXPERIMENT_NAME}_${DATASET_NAME}_$(basename ${MODEL_PATH})"}
+
+
+# 1. Prepare the dataset
+export HF_ENDPOINT="https://hf-mirror.com"
+
+train_file="./data/${DATASET_NAME}/train.parquet"
+val_file="./data/${DATASET_NAME}/test.parquet"
+python3 -m src.data_preprocess.${DATASET_NAME}.${EXPERIMENT_NAME} --local_dir ./data/${DATASET_NAME}
+
+
+# 2. Perform the training
+export VERL_USE_MODELSCOPE=True
+python3 -m src.train \
+    algorithm.adv_estimator=grpo \
+    algorithm.use_kl_in_reward=False \
+    data.train_files=$train_file \
+    data.val_files=$val_file \
+    data.train_batch_size=256 \
+    data.max_prompt_length=4096 \
+    data.max_response_length=4096 \
+    data.filter_overlong_prompts=True \
+    data.truncation='left' \
+    custom_reward_function.path=./src/reward_score/${DATASET_NAME}.py \
+    custom_reward_function.name=compute_${EXPERIMENT_NAME}_score \
+    actor_rollout_ref.model.path=${MODEL_PATH} \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=False \
+    trainer.logger=['console'] \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=30 \
+    trainer.test_freq=1 \
+    trainer.default_local_dir=${SAVE_CHECKPOINT_PATH} \
+    trainer.total_epochs=3 $@
